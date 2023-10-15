660110cda3cbb20efee46b341be69765bf50d778
Boon Wee <bwtan08@gmail.com>
2023-10-15 23:07:21 +0800

    changes to the CI Asm

--- CI_ASM1.py
+++ CI_ASM1.py
@@ -32,26 +32,135 @@ river_water_level.view()
 no_of_trees_planted.view()
 flood_warning_level.view()
 
-# rule1 = ctrl.Rule(rainfall_intensity['low'] & river_water_level['low'] & no_of_trees['dry'], flood_warning_level['low'])
-# rule2 = ctrl.Rule(rainfall_intensity['moderate'] & river_water_level['moderate'] & no_of_trees['moist'], flood_warning_level['moderate'])
-# rule3 = ctrl.Rule(rainfall_intensity['high'] | river_water_level['high'] | no_of_trees['wet'], flood_warning_level['high'])
+# Rain Intensity & River Water Level
+rule1 = ctrl.Rule(rainfall_intensity['light'] & river_water_level['normal'], flood_warning_level['low'])
+rule2 = ctrl.Rule(rainfall_intensity['light'] & river_water_level['alert'], flood_warning_level['moderate'])
+rule3 = ctrl.Rule(rainfall_intensity['moderate'] & river_water_level['normal'], flood_warning_level['low'])
+rule4 = ctrl.Rule(rainfall_intensity['moderate'] & river_water_level['alert'], flood_warning_level['moderate'])
+rule5 = ctrl.Rule(rainfall_intensity['heavy'] & river_water_level['warning'], flood_warning_level['moderate'])
+rule6 = ctrl.Rule(rainfall_intensity['heavy'] & river_water_level['danger'], flood_warning_level['high'])
+rule7 = ctrl.Rule(rainfall_intensity['very heavy'] & river_water_level['warning'], flood_warning_level['moderate'])
+rule8 = ctrl.Rule(rainfall_intensity['very heavy'] & river_water_level['danger'], flood_warning_level['high'])
 
-# rules = [rule1, rule2, rule3]
-# print (rules)
+# Rain Intensity & Number of Trees Planted
+rule9 = ctrl.Rule(rainfall_intensity['light'] & no_of_trees_planted['few'], flood_warning_level['moderate'])
+rule10 = ctrl.Rule(rainfall_intensity['light'] & no_of_trees_planted['moderate'], flood_warning_level['low'])
+rule11 = ctrl.Rule(rainfall_intensity['light'] & no_of_trees_planted['many'], flood_warning_level['low'])
+rule12 = ctrl.Rule(rainfall_intensity['moderate'] & no_of_trees_planted['few'], flood_warning_level['moderate'])
+rule13 = ctrl.Rule(rainfall_intensity['moderate'] & no_of_trees_planted['moderate'], flood_warning_level['low'])
+rule14 = ctrl.Rule(rainfall_intensity['moderate'] & no_of_trees_planted['many'], flood_warning_level['low'])
+rule15 = ctrl.Rule(rainfall_intensity['heavy'] & no_of_trees_planted['few'], flood_warning_level['high'])
+rule16 = ctrl.Rule(rainfall_intensity['heavy'] & no_of_trees_planted['moderate'], flood_warning_level['moderate'])
+rule17 = ctrl.Rule(rainfall_intensity['heavy'] & no_of_trees_planted['many'], flood_warning_level['low'])
+rule18 = ctrl.Rule(rainfall_intensity['very heavy'] & no_of_trees_planted['few'], flood_warning_level['high'])
+rule19 = ctrl.Rule(rainfall_intensity['very heavy'] & no_of_trees_planted['moderate'], flood_warning_level['moderate'])
+rule20 = ctrl.Rule(rainfall_intensity['very heavy'] & no_of_trees_planted['many'], flood_warning_level['moderate'])
 
-# train_ctrl = ctrl.ControlSystem(rules = rules)
+# River Water Level & Number of Trees Planted
+rule21 = ctrl.Rule(river_water_level['normal'] & no_of_trees_planted['few'], flood_warning_level['low'])
+rule22 = ctrl.Rule(river_water_level['normal'] & no_of_trees_planted['moderate'], flood_warning_level['low'])
+rule23 = ctrl.Rule(river_water_level['normal'] & no_of_trees_planted['many'], flood_warning_level['low'])
+rule24 = ctrl.Rule(river_water_level['alert'] & no_of_trees_planted['few'], flood_warning_level['moderate'])
+rule25 = ctrl.Rule(river_water_level['alert'] & no_of_trees_planted['moderate'], flood_warning_level['moderate'])
+rule26 = ctrl.Rule(river_water_level['alert'] & no_of_trees_planted['many'], flood_warning_level['moderate'])
+rule27 = ctrl.Rule(river_water_level['warning'] & no_of_trees_planted['few'], flood_warning_level['high'])
+rule28 = ctrl.Rule(river_water_level['warning'] & no_of_trees_planted['moderate'], flood_warning_level['high'])
+rule29 = ctrl.Rule(river_water_level['warning'] & no_of_trees_planted['many'], flood_warning_level['high'])
+rule30 = ctrl.Rule(river_water_level['danger'] & no_of_trees_planted['few'], flood_warning_level['high'])
+rule31 = ctrl.Rule(river_water_level['danger'] & no_of_trees_planted['moderate'], flood_warning_level['high'])
+rule32 = ctrl.Rule(river_water_level['danger'] & no_of_trees_planted['many'], flood_warning_level['high'])
 
-# train = ctrl.ControlSystemSimulation(control_system = train_ctrl)
+rules = [rule1, rule2, rule3, rule4, rule5,rule6, rule7, rule8, rule9, rule10, rule11, rule12, rule13, rule14, rule15, rule16, rule17, rule18, rule19, rule20,
+         rule21, rule22, rule23, rule24, rule25, rule26, rule27, rule28, rule29, rule30, rule31, rule32]
 
-# train.input['rainfall_intensity'] = 30
-# train.input['river_water_level'] = 50
-# train.input['soil_moisture'] = 70
+# Construct the fuzzy control system
+train_ctrl = ctrl.ControlSystem(rules=rules)
 
-# train.compute()
+train = ctrl.ControlSystemSimulation(control_system=train_ctrl)
 
-# print(train.output)
+# define the values for the inputs
+train.input['rainfall_intensity'] = 60
+train.input['river_water_level'] = 27
+train.input['no_of_trees_planted'] = 500
 
-# print(train.output['flood_warning_level'])
+# compute the outputs
+train.compute()
 
-# flood_warning_level.view(sim=train)
+# print the output values
+print(train.output)
+
+# to extract one of the outputs
+print(train.output['flood_warning_level'])
+
+flood_warning_level.view(sim=train)
+
+# Rain Intensity & River Water Level
+# ==========================================================================
+# x, y = np.meshgrid(np.linspace(river_water_level.universe.min(), river_water_level.universe.max(), 100),
+#                    np.linspace(rainfall_intensity.universe.min(), rainfall_intensity.universe.max(), 100))
+# z_flood_warning_level = np.zeros_like(x, dtype=float)
+
+
+# for i,r in enumerate(x):
+#   for j,c in enumerate(r):
+#     train.input['river_water_level'] = x[i,j]
+#     train.input['rainfall_intensity'] = y[i,j]
+#     try:
+#       train.compute()
+#     except:
+#       z_flood_warning_level[i,j] = float('inf')
+#     z_flood_warning_level[i,j] = train.output['flood_warning_level']
+
+# Rain Intensity & Number of Trees Planted
+# ==========================================================================
+# x, y = np.meshgrid(np.linspace(no_of_trees_planted.universe.min(), no_of_trees_planted.universe.max(), 100),
+#                    np.linspace(rainfall_intensity.universe.min(), rainfall_intensity.universe.max(), 100))
+# z_flood_warning_level = np.zeros_like(x, dtype=float)
+
+
+# for i,r in enumerate(x):
+#   for j,c in enumerate(r):
+#     train.input['no_of_trees_planted'] = x[i,j]
+#     train.input['rainfall_intensity'] = y[i,j]
+#     try:
+#       train.compute()
+#     except:
+#       z_flood_warning_level[i,j] = float('inf')
+#     z_flood_warning_level[i,j] = train.output['flood_warning_level']
+
+# River Water Level & Number of Trees Planted
+# ==========================================================================
+x, y = np.meshgrid(np.linspace(no_of_trees_planted.universe.min(), no_of_trees_planted.universe.max(), 100),
+                    np.linspace(river_water_level.universe.min(), river_water_level.universe.max(), 100))
+z_flood_warning_level = np.zeros_like(x, dtype=float)
+
+
+for i,r in enumerate(x):
+  for j,c in enumerate(r):
+    train.input['no_of_trees_planted'] = x[i,j]
+    train.input['river_water_level'] = y[i,j]
+    try:
+      train.compute()
+    except:
+      z_flood_warning_level[i,j] = float('inf')
+    z_flood_warning_level[i,j] = train.output['flood_warning_level']
+   
+    
+import matplotlib.pyplot as plt
+# from mpl_toolkits.mplot3d import Axes3D
+
+def plot3d(x,y,z):
+  fig = plt.figure()
+  ax = fig.add_subplot(111, projection='3d')
+
+  ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap='viridis', linewidth=0.4, antialiased=True)
+
+  ax.contourf(x, y, z, zdir='z', offset=-2.5, cmap='viridis', alpha=0.5)
+  ax.contourf(x, y, z, zdir='x', offset=x.max()*1.5, cmap='viridis', alpha=0.5)
+  ax.contourf(x, y, z, zdir='y', offset=y.max()*1.5, cmap='viridis', alpha=0.5)
+
+  ax.view_init(30, 200)
+
+plot3d(x, y, z_flood_warning_level)
+
     
