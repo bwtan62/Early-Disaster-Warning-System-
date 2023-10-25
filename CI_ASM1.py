b8b4bdbd2feec9ccc5722040c924565c994b96d2
Lee Jia Qian <leejiaqian02@gmail.com>
2023-10-26 00:17:24 +0800

    new changes

--- CI_ASM1.py
+++ CI_ASM1.py
@@ -3,13 +3,15 @@ import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 import matplotlib.pyplot as plt
 
-
+# Input variables
 rainfall_intensity = ctrl.Antecedent(np.arange(0, 60, 0.1), 'rainfall_intensity')
 river_water_level = ctrl.Antecedent(np.arange(0, 27, 0.1), 'river_water_level')
 no_of_trees_planted = ctrl.Antecedent(np.arange(0, 500, 1), 'no_of_trees_planted')
 
+# Output variable
 flood_warning_level = ctrl.Consequent(np.arange(0, 100, 1), 'flood_warning_level')
 
+# Membership function
 rainfall_intensity['light'] = fuzz.trimf(rainfall_intensity.universe, [0, 0, 15])
 rainfall_intensity['moderate'] = fuzz.trimf(rainfall_intensity.universe, [10, 20, 35])
 rainfall_intensity['heavy'] = fuzz.trimf(rainfall_intensity.universe,[25, 45, 60])
@@ -28,6 +30,7 @@ flood_warning_level['low'] = fuzz.trimf(flood_warning_level.universe, [0, 25, 50
 flood_warning_level['moderate'] = fuzz.trimf(flood_warning_level.universe, [20, 50, 80])
 flood_warning_level['high'] = fuzz.trimf(flood_warning_level.universe, [50, 100, 100])
 
+# Graph output
 rainfall_intensity.view()
 river_water_level.view()
 no_of_trees_planted.view()
@@ -73,31 +76,29 @@ rules = [rule1, rule2, rule3, rule4, rule5,rule6, rule7, rule8, rule9, rule10, r
          rule21, rule22, rule23, rule24, rule25, rule26, rule27, rule28, rule29, rule30]
 
 # Construct the fuzzy control system
-train_ctrl = ctrl.ControlSystem(rules=rules)
+flood_ctrl = ctrl.ControlSystem(rules=rules)
+flood = ctrl.ControlSystemSimulation(control_system=flood_ctrl)
 
-train = ctrl.ControlSystemSimulation(control_system=train_ctrl)
+# Calculate output
+def flood_out(rainfall_intensity, river_water_level, no_of_trees_planted):
+    # define the values for the inputs
+    flood.input['rainfall_intensity'] = rainfall_intensity
+    flood.input['river_water_level'] = river_water_level
+    flood.input['no_of_trees_planted'] = no_of_trees_planted
+    
+    # compute the outputs
+    flood.compute()
 
-# define the values for the inputs
-# train.input['rainfall_intensity'] = int(input("Please enter rain intensity from 0 to 60mm: "))
-# train.input['river_water_level'] = int(input("Please enter river water level from 0 to 27m: "))
-# train.input['no_of_trees_planted'] = int(input("Please enter number of tree planted from 0 to 500k: "))
+    # print the output values
+    print(flood.output)
 
-train.input['rainfall_intensity'] = 21
-train.input['river_water_level'] = 24.6
-train.input['no_of_trees_planted'] = 10
+    # to extract one of the outputs
+    print("Flood Warning Level: ", flood.output['flood_warning_level'])
 
-# compute the outputs
-train.compute()
+    flood_warning_level.view(sim=flood)
 
-# print the output values
-print(train.output)
-
-# to extract one of the outputs
-print("Flood Warning Level: ", train.output['flood_warning_level'])
-
-flood_warning_level.view(sim=train)
-
-def generate3D(input1, input2, train, title):
+# 3D visualisation
+def generate3D(input1, input2, flood, title):
     x, y = np.meshgrid(
         np.linspace(input1.universe.min(), input1.universe.max(), 100),
         np.linspace(input2.universe.min(), input2.universe.max(), 100)
@@ -106,13 +107,13 @@ def generate3D(input1, input2, train, title):
     
     for i,r in enumerate(x):
         for j,c in enumerate(r):
-          train.input[input1.label] = x[i,j]
-          train.input[input2.label] = y[i,j]
+          flood.input[input1.label] = x[i,j]
+          flood.input[input2.label] = y[i,j]
           try:
-            train.compute()
+            flood.compute()
           except:
             z[i,j] = float('inf')
-          z[i,j] = train.output['flood_warning_level']
+          z[i,j] = flood.output['flood_warning_level']
     
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
@@ -129,11 +130,38 @@ def generate3D(input1, input2, train, title):
     ax.view_init(30, 200)
     ax.set_title(title)
     plt.show()
+    
+# Scenario 1
+print("Scenario 1:\nrain intensity = 6mm/h, river water level = 23 meter, number of tree planted = 10k")
+flood_out(6, 23, 10)
+
+# Scenario 2
+print("Scenario 2:\nrain intensity = 21mm/h, river water level = 24.6 meter, number of tree planted = 10k")
+flood_out(21, 24.6, 10)
+
+# Scenario 3
+print("Scenario 3:\nrain intensity = 6mm/h, river water level = 23 meter, number of tree planted = 10k")
+flood_out(6, 23, 10)
+
+# Scenario 4
+print("Scenario 4:\nrain intensity = 6mm/h, river water level = 23 meter, number of tree planted = 10k")
+flood_out(6, 23, 10)
+
+# User input
+# ri = int(input("Please enter rain intensity from 0 to 60mm (0 >= 60): "))
+# rwl = int(input("Please enter river water level from 0 to 27m (0 >= 27): "))
+# num_tp = int(input("Please enter number of tree planted from 0 to 500k (0 >= 500): "))
+# flood_out(ri, rwl, num_tp)
 
 # Generate and display the 3D plots
-generate3D(river_water_level, rainfall_intensity, train, "Rain Intensity & River Water Level")
-generate3D(no_of_trees_planted, rainfall_intensity, train, "Rain Intensity & Number of Trees Planted")
-generate3D(no_of_trees_planted, river_water_level, train, "River Water Level & Number of Trees Planted")
+generate3D(river_water_level, rainfall_intensity, flood, "Rain Intensity & River Water Level")
+generate3D(no_of_trees_planted, rainfall_intensity, flood, "Rain Intensity & Number of Trees Planted")
+generate3D(no_of_trees_planted, river_water_level, flood, "River Water Level & Number of Trees Planted")
+
+
+
+
+
 
 
 
